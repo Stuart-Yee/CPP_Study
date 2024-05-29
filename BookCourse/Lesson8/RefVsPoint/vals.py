304eb08951e3ef4671457d46bdd6a36a48c2ec83
Stuart-Yee <ki6fzb@gmail.com>
2024-05-28 21:18:55 -0700

    did some comparison to Python

--- BookCourse/Lesson8/RefVsPoint/vals.py
+++ BookCourse/Lesson8/RefVsPoint/vals.py
@@ -1,4 +1,4 @@
-def doubleIt(x):
+def doubleIt(x: int) -> int:
     x *= 2
     return x
 
@@ -9,4 +9,18 @@ print(f"Doubling A: {doubleIt(a)}")
 
 print(f"A is {a}")
 
-# In conclusion, Python passes arguments by value
+# In conclusion, Python passes arguments by value for integers
+
+def swapIt(a: list) -> list:
+    first = a[0]
+    a[0] = a[-1]
+    a[-1] = first
+    return a
+
+my_list = ["potato", "strawberry", "avalanche"]
+
+print(f"My list:\n{my_list}")
+print(f"Swap it:\n{swapIt(my_list)}")
+print(f"My list:\n{my_list}")
+
+#BUT! Lists are passed by reference
